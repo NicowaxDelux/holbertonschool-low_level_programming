9ebe1d717e376c95b67fe6ebc256885d35114a17
NicowaxDelux <nycko111995@gmail.com>
2024-02-21 03:35:08 +0000

    se realizo codigo C that printf the size of various types, 6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
+
+	printf("Size of char: %zu bytes\n", sizeof(charType));
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of long int: %zu bytes\n", sizeof(longintType));
+	printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
+	printf("Size of float: %zu byte\n", sizeof(floatType));
+
+	return (0);
+}
