07f4356b5c8dedcade45548582ef656401c8c27f
NicowaxDelux <nycko111995@gmail.com>
2024-02-21 23:53:32 +0000

    respuesta punto 0-positive_or-negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -18,16 +18,14 @@ int main(void)
 
 	if (n == 0)
 	{
-		printf(" is zero\n");
+		printf("%d is zero\n", n);
 	}
 	else
 	{
 		if (n < 0)
-			printf(" is negative\n");
+			printf("%d is negative\n", n);
 		else
-		{
-			printf(" is positive\n");
-		}
+			printf("%d is positive\n", n);
 	}
 	return (0);
 }
