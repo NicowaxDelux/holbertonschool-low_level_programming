b2bc718e4b46073a2b48a62fef673fb1698a2796
NicowaxDelux <nycko111995@gmail.com>
2024-02-21 22:53:38 +0000

    se crea el directorio, despues re realiza el punto 0-positive or negative y se compila el archivo con gcc y luego se ejeucta con betty,despues se sube

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n == 0)
+	{
+		printf(" is zero\n");
+	}
+	else
+	{
+		if (n < 0)
+			printf(" is negative\n");
+		else
+		{
+			printf(" is positive\n");
+		}
+	}
+	return (0);
+}
