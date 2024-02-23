c8116e3a579fab95cbd63b0b961ee1745215e6e6
NicowaxDelux <nycko111995@gmail.com>
2024-02-23 14:53:44 -0500

    se imprimio el alfabeto con _putchar y con ciclo for

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ *
+*/
+void print_alphabet(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+
+		_putchar(abc);
+
+	_putchar('\n');
+}
