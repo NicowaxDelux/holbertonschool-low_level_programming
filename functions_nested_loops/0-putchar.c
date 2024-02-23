ed9fee8249912a1355186b3c84acb608c636f137
NicowaxDelux <nycko111995@gmail.com>
2024-02-23 11:30:44 -0500

    se imprime _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Prints a _putchar, followed by a new line.
+ *
+ * Return: Always 0.
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
