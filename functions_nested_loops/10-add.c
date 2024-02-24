6fe8017096a465e6a82ba07432febdda83cdaed0
NicowaxDelux <nycko111995@gmail.com>
2024-02-23 23:53:58 -0500

    se hizo return de la suma tomando los numeros especificos dentro de la funcion

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result.
+ * @number1: The first integer to be added
+ * @number2: The second integer to be added
+ *
+ * Return: The result of the addition
+ */
+int add(int number1, int  number2)
+{
+	return (number1 + number2);
+}
