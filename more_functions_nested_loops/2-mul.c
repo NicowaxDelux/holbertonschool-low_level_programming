22c09a9dde87564709a063bc2e9ead53e89238d1
NicowaxDelux <nycko111995@gmail.com>
2024-02-26 17:56:44 -0500

    retorna multiplicacion

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - unction that multiplies two integers.
+ *@a: value a
+ *@b: value b
+ *
+ * Return: return value multiplied by a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
