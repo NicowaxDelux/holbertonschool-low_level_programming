3f01debfee8cd0ac4d177357049355d7bb0ce663
NicowaxDelux <nycko111995@gmail.com>
2024-02-27 18:00:14 -0500

    se uso preposicion if

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number -  prints an integer.
+ *
+ * @n:integer value
+ */
+void print_number(int n)
+{
+	int number = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
+
+	if ((number / 10) > 0)
+
+		print_number(number / 10);
+
+	_putchar ((number % 10) + '0');
+}
