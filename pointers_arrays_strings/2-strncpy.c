49be3a09103c1a5fa1021cfc4fc87af9ce608c1d
NicowaxDelux <nycko111995@gmail.com>
2024-03-01 15:50:55 -0500

    se uso dos for y un while

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  *_strncpy - write a function that copies a string.
+  *
+  *@dest: string value
+  *@src: string value
+  *@n: new bytes
+  *
+  *Return: return string with strncpy
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int count = 0, lenght = 0;
+
+	while (dest[count++])
+
+		lenght++;
+
+	for (count = 0; src[count] && count < n; count++)
+
+		dest[count] = src[count];
+
+	for (count = lenght; count < n; count++)
+
+		dest[count] = '\0';
+
+	return (dest);
+}
