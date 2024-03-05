5c39570c0e9e935ccdac29430ab964295c0c84de
NicowaxDelux <nycko111995@gmail.com>
2024-03-05 18:43:00 -0500

    se uso ciclo for

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  *_memcpy - function that copies memory area
+  *
+  *@dest: destination to which the bytes will be copied
+  *@src: memory area
+  *@n: number bytes to copy
+  *
+  *Return: returns a pointer to dest
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+
+	return (dest);
+}
