c519db0c9a044f55115f609bbff8558c57de2be9
NicowaxDelux <nycko111995@gmail.com>
2024-03-06 20:02:41 -0500

    se uso el ciclo for y preposicion if

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  *_strstr - function finds the first occurrence of the substring needle
+  *in the string haystack. The terminating null bytes (\0) are not compared
+  *
+  *@haystack: String
+  *@needle: finds the first occurrence of the substring
+  *
+  *Return: Returns a pointer to the beginning of the located substring,
+  *or NULL if the substring is not found.
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	while (*haystack)
+	{
+		for (i = 0; needle[i]; i++)
+		{
+
+			if ((needle[i] == haystack[i]) != '\0')
+
+				return (needle);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
