4d0f44b78460c91868b4b202060d6f1fd6fffb17
NicowaxDelux <nycko111995@gmail.com>
2024-03-14 10:38:23 -0500

    respuesta 3-alloc_grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * alloc_grid - Write a function that returns a pointer
+  * to a 2 dimensional array of integers.
+  *
+  * @width: width of the grid
+  * @height: height of the grid
+  *
+  *Return: The function should return NULL on failure
+  */
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i = 0, j = 0;
+
+	if (width <= 0 || height <= 0)
+
+		return (NULL);
+
+	array = (int **) malloc(sizeof(int *) * height);
+
+	if (array != NULL)
+	{
+		for (; i < height; i++)
+		{
+			array[i] = (int *) malloc(sizeof(int) * width);
+
+			if (array[i] != NULL)
+			{
+				for (; j < width; j++)
+				{
+					array[i][j] = 0;
+				}
+			}
+			else
+			{
+				if (i <= 0)
+				{
+					free(array[i]);
+					i--;
+				}
+				free(array);
+				return (NULL);
+			}
+		}
+		return (array);
+	}
+	else
+		return (NULL);
+
+}
