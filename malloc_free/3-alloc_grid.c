23b76428a867bfb0f3153b3b2cf0ef1a6e5d430a
NicowaxDelux <nycko111995@gmail.com>
2024-03-14 11:51:16 -0500

    respuesta 3-alloc_grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,20 +14,22 @@
 int **alloc_grid(int width, int height)
 {
 	int **array;
-	int i = 0, j = 0;
+	int i = 0, j;
 
 	if (width == 0 || height == 0)
 		return (NULL);
-
 	array = (int **) malloc(sizeof(int *) * height);
-
 	if (array != NULL)
 	{
 		for (; i < height; i++)
 		{
 			array[i] = (int *) malloc(sizeof(int) * width);
-
 			if (array[i] != NULL)
+			{
+				for (j = 0; j < width; j++)
+					array[i][j] = 0;
+			}
+			else
 			{
 				while (i >= 0)
 				{
@@ -37,11 +39,6 @@ int **alloc_grid(int width, int height)
 				free(array);
 				return (NULL);
 			}
-
-			for (; j < width; j++)
-
-				array[i][j] = 0;
-
 		}
 		return (array);
 	}
