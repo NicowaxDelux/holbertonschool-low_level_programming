f404b7bca724ca268c8c5d5ca127fd69665ffd4e
NicowaxDelux <nycko111995@gmail.com>
2024-03-20 18:30:05 -0500

    calculadora con soperaciones sencillas

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * op_add - funtion that return sum
+  *@a: num1
+  *@b: num2
+  *
+  *Return: return sum
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  *op_sub - funtion that return subtraction
+  *
+  *@a: num1
+  *@b: num2
+  *Return: return subtraction
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  *op_mul - funtion that return product
+  *
+  *@a: num1
+  *@b: num2
+  *
+  *Return: return product
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  *op_div - funtion that return division
+  *
+  *@a: num1
+  *@b: num2
+  *
+  *Return: return division
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  *op_mod -  funtion that return remainder of the division
+  *
+  *@a: num1
+  *@b: num2
+  *
+  *Return: return remainder of the division
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
