aee02b55b8fe03b465aa7d000d858743c345089a
NicowaxDelux <nycko111995@gmail.com>
2024-04-02 14:41:01 -0500

    se agrega prototipos y estructura

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  *
+  *
+  *
+  *
+  *
+  *
+  */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
