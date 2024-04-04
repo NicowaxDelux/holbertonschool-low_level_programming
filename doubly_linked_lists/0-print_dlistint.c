9120f8c6c0f00e1042c59060b6b3a2e422eddb73
NicowaxDelux <nycko111995@gmail.com>
2024-04-03 21:09:45 -0500

    header lists.h is added with structure and prototypes and funtion tha print all the elements of the list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * print_dlistint - function that prints all
+  * the elements of a dlistint_t list.
+  *
+  * @h: list of head
+  *
+  * Return: return count of nodes
+  *
+  */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
