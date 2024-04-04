26824d37b13f21f299da557371dbc12deb4136b6
NicowaxDelux <nycko111995@gmail.com>
2024-04-04 02:02:46 -0500

    unction that adds a new node at the end of a dlistint_t list.

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,6 +24,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->n = n;
 
 	new_node->next = NULL;
+	new_node->prev = NULL;
 
 	last_node = *head;
 
@@ -36,7 +37,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			last_node = last_node->next;
 
 		last_node->next = new_node;
-		new_node->prev = *head;
+		new_node->prev = last_node;
 
 	}
 	return (*head);
