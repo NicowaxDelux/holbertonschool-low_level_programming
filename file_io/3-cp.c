29259da5bdca1f9765d7150d93c5766453ebcc08
NicowaxDelux <nycko111995@gmail.com>
2024-04-05 23:07:48 -0500

    program that copies the content of a file to another file.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,7 +18,7 @@ void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE%s\n)",
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE%s\n",
 				argv[1]);
 		exit(98);
 	}
