0ca26cde0deec50e83f9c378a0e461dc5b78e304
NicowaxDelux <nycko111995@gmail.com>
2024-04-05 19:29:29 -0500

    Create a function that creates a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+   * create_file - Create a function that creates a file
+   *
+   * @filename: new file,is the name of the file to create
+   *
+   * @text_content: is a NULL terminated string to write to the file
+   *
+   * Return: return the file create
+   */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, rwf;
+	int lenletters;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (lenletters = 0; text_content[lenletters]; lenletters++)
+		;
+
+	rwf = write(fd, text_content, lenletters);
+
+	if (rwf == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+
+}
